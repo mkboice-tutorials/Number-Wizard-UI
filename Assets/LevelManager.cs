ea59f9c3d0c146c9b01f14b3d79b75629e5857d4
Mike Boice <mike.boice@netapp.com>
2017-07-08 12:20:18 -0600

    Add button scripts

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void loadLevel(string name)
+	{
+		Debug.Log("Level load requested for:" + name);
+	}
+
+	public void quitRequest()
+	{
+		Debug.Log("I want to quit");
+	}
+
+}

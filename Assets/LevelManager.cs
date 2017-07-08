6cb61f0dea11178af5b9a59e48facf81cfb03f90
Mike Boice <mike.boice@netapp.com>
2017-07-08 12:43:32 -0600

    Add change scene on button press

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -8,11 +9,13 @@ public class LevelManager : MonoBehaviour
 	public void loadLevel(string name)
 	{
 		Debug.Log("Level load requested for:" + name);
+		SceneManager.LoadScene(name);
 	}
 
 	public void quitRequest()
 	{
 		Debug.Log("I want to quit");
+		Application.Quit();
 	}
 
 }

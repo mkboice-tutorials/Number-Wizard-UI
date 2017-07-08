3b8734137ae89cd0996bbead1e4046050abd7b8b
Mike Boice <mike.boice@netapp.com>
2017-07-08 13:30:22 -0600

    Add number wizard logic

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    public Text guessText;
+    int max;
+    int min;
+    int guess;
+    public int maxGuessesAllowed;
+
+    public Text text;
+
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        maxGuessesAllowed = 10;
+
+        max = max + 1;
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void GuessCorrect()
+    {
+        SceneManager.LoadScene("Win");
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+}
